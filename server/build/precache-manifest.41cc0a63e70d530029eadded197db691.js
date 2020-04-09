37fa3849d1f333dd80e653b87bd0908ee9d8e2c2
Matteo Mortelliti <mortellitimatteo@gmail.com>
2020-04-09 08:01:38 -0400

    with build working with server

---
+++ server/build/precache-manifest.41cc0a63e70d530029eadded197db691.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e06f52c3beb8dda2685ce42555582f70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2471b7d613d0665de173",
+    "url": "/static/css/main.8bb5b585.chunk.css"
+  },
+  {
+    "revision": "29847b7b0d8afd4bd044",
+    "url": "/static/js/2.dcceaf90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dcceaf90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2471b7d613d0665de173",
+    "url": "/static/js/main.c2bd4149.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "c1b4e74c297be2c369aef68696168c1a",
+    "url": "/static/media/logo.c1b4e74c.svg"
+  }
+]);
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 
-const PORT = 31415;
+const PORT = process.env.PORT || 31415;
 
 var app = express();
 
@@ -12,6 +12,17 @@ app.use(require('./routes/tweet'));
 app.use(require('./routes/feed'));
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 
 const server = app.listen(PORT, function() {
   console.info('🌍 Listening on port ' + server.address().port);
